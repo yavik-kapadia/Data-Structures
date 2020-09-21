06e61ba45b1114a5e21b904fe675850b020ff912
NixMelos <yavikkapadia@gmail.com>
2020-09-21 16:20:16 -0700

    Started recursion project

--- Recursion/src/Recursion.java
+++ Recursion/src/Recursion.java
@@ -1,6 +1,5 @@
-import javax.print.DocFlavor;
 import java.util.Scanner;
-import java.util.SplittableRandom;
+
 
 /**
  * A program to display a user provided string in reverse order using recursion
@@ -11,35 +10,81 @@ import java.util.SplittableRandom;
  */
 public class Recursion {
     /**
-     * Main method to run the program
+     * Main method to test the program
      */
     public static void main(String[] args) {
-        String original;
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter your string: ");
-        original = input.nextLine();
-        reverseDisplay(original);
 
+        System.out.println("Testing Reverse order recursion!");
+        System.out.println();
+        System.out.print("Enter your string: ");
+        String value = input.nextLine();
+        reverseDisplay(value);
+        System.out.println();
+
+        System.out.println("Testing Permutations!");
+        System.out.println();
+        System.out.print("Enter your string: ");
+        String s = input.nextLine();
+        displayPermutation(s);
+        System.out.println();
 
     }
 
     /**
-     * a method to reverse the order of the string
+     * a method that print string in reverse order
      *
      * @param value string to be reversed
      */
     public static void reverseDisplay(String value) {
+
         reverseDisplay(value, value.length() - 1);
     }
+
 
     /**
      * helper method for reversing a string
      *
-     * @param value
-     * @param high
+     * @param value string to be processed
+     * @param high index of the substring
      */
     public static void reverseDisplay(String value, int high) {
+        if (high >= 0) {
+            System.out.print(value.charAt(high));
+            reverseDisplay(value, high - 1);
+        }
 
     }
+
+    /**
+     * Method to print all permutations of a string
+     * @param s string whose permutations are being displayed
+     */
+    public static void displayPermutation(String s){
+
+        displayPermutation("", s);
+    }
+
+    /**
+     * Helper method for generating per
+     * @param s1 is the string where characters from s2 are moved to.
+     * @param s2 provides the string whose permutations are being processed and characters are being removed from.
+     */
+    public static void displayPermutation(String s1, String s2){
+        if((s2 == null)|| s2.length() == 0) {
+
+            System.out.println(s1);
+        }
+        else{
+            for (int i = 0; i < s2.length(); i++) {
+
+                String new_s1 = s1 + s2.charAt(i) ;
+                String new_s2 = s2.substring(0, i) + s2.substring(i + 1);
+                displayPermutation(new_s1, new_s2);
+
+
+            }
+        }
+    }
+
 }
