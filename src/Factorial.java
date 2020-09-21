b7ce8fc2baae9c41e8aef887263742f687d1bacb
Nix Melos <yavikkapadia@gmail.com>
2020-09-20 23:36:02 -0700

    Practicing Recursion

---
+++ src/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        System.out.println(fact(5));
+    }
+    public static int fact(int n)
+    {
+        if(n >=1)
+        {
+            return n * fact(n - 1);
+        }
+        else
+            return 1;
+
+    }
+}
+
