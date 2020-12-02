606a7b2d829fe579689e3266c1236b6122baa52f
NixMelos <yavikkapadia@gmail.com>
2020-12-01 23:18:23 -0800

    Completed bst

--- BST/src/BST.java
+++ BST/src/BST.java
@@ -1,39 +1,247 @@
+import java.util.Arrays;
 import java.util.Collection;
 
+
+interface Tree<E> extends Collection<E> {
+    /**
+     * Return true if the element is in the tree
+     */
+    boolean search(E e);
+
+    /**
+     * Insert element e into the binary tree
+     * Return true if the element is inserted successfully
+     */
+    boolean insert(E e);
+
+    /**
+     * Delete the specified element from the tree
+     * Return true if the element is deleted successfully
+     */
+    boolean delete(E e);
+
+    /**
+     * Get the number of elements in the tree
+     */
+    int getSize();
+
+    /**
+     * Inorder traversal from the root
+     */
+    default void inorder() {
+    }
+
+    /**
+     * Postorder traversal from the root
+     */
+    default void postorder() {
+    }
+
+    /**
+     * Preorder traversal from the root
+     */
+    default void preorder() {
+    }
+
+    @Override
+    /** Return true if the tree is empty */
+    default boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    @Override
+    default boolean contains(Object e) {
+        return search((E) e);
+    }
+
+    @Override
+    default boolean add(E e) {
+        return insert(e);
+    }
+
+    @Override
+    default boolean remove(Object e) {
+        return delete((E) e);
+    }
+
+    @Override
+    default int size() {
+        return getSize();
+    }
+
+    @Override
+    default boolean containsAll(Collection<?> c) {
+        for (Object e : c) {
+            if (!this.contains(e)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    default boolean addAll(Collection<? extends E> c) {
+        boolean changed = false;
+        for (E element : c) {
+            if (this.add(element)) {
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    @Override
+    default boolean removeAll(Collection<?> c) {
+        boolean changed = false;
+        for (Object o : c) {
+            if (this.remove(o)) {
+                changed = true;
+            }
+        }
+        return changed;
+    }
+
+    @Override
+    default boolean retainAll(Collection<?> c) {
+        boolean changed = false;
+        for (E e : this) {
+            if (!c.contains(e)) {
+                remove(e);
+            }
+        }
+        return true;
+
+    }
+
+
+    @Override
+    default Object[] toArray() {
+        Object[] arr = new Object[this.getSize()];
+        int index = 0;
+        for (E e : this) {
+            arr[index] = e;
+            index++;
+        }
+        return arr;
+
+    }
+
+    @Override
+    default <T> T[] toArray(T[] array) {
+        // Left as an exercise
+        array = (T[]) new Object[this.getSize()];
+        int index = 0;
+        for (E e : this) {
+            array[index] = (T) e;
+            index++;
+        }
+        return array;
+    }
+}
 
 public class BST<E extends Comparable<E>> implements Tree<E> {
     protected TreeNode<E> root;
     protected int size = 0;
 
-    /** Create a default binary tree */
+    public static void main(String[] args) {
+        System.out.println("Testing BST");
+        BST<String> tree1 = new BST<>();
+        System.out.println("Is tree1 empty? " + tree1.isEmpty());
+        tree1.insert("George");
+        tree1.insert("Porter");
+        tree1.insert("Jeff");
+        tree1.insert("Michael");
+        tree1.insert("Adam");
+        tree1.insert("Tracy");
+        System.out.print("Inorder (Sorted): ");
+        tree1.inorder();
+        System.out.print("\nPostorder: ");
+        tree1.postorder();
+        System.out.print("\nPreorder: ");
+        tree1.preorder();
+        System.out.print("\n The number of nodes is " + tree1.getSize());
+        System.out.print("\nIs Peter in the tree1? " + tree1.search("Peter"));
+        System.out.print("\nIs Jeff in the tree1? " + tree1.search("Jeff"));
+        System.out.print("\nA path from the root to Jeff is: ");
+        java.util.ArrayList<BST.TreeNode<String>> path = tree1.path("Jeff");
+        for (int i = 0; path != null && i < path.size(); i++)
+            System.out.print(path.get(i).element + " ");
+        System.out.println();
+        System.out.println("Removing Adam from tree1.");
+        tree1.delete("Adam");
+        System.out.print("Current State of Tree1: ");
+        tree1.inorder();
+        System.out.print("\nTesting Array to BST: ");
+        Integer[] numbers = {2, 4, 3, 1, 8 , 5, 6 , 7};
+        System.out.print("\nArray of numbers: "+ Arrays.toString(numbers));
+        BST<Integer> intTree = new BST<>(numbers);
+        System.out.print("\nNumbers inOrder (Sorted): ");
+        intTree.inorder();
+        System.out.print("\nDoes intTree contain 3? " + intTree.contains(3));
+        System.out.print("\nTesting set manipulation: ");
+        BST<String> tree2 =  new BST<>();
+        tree2.insert("Jenny");
+        tree2.insert("Jeff");
+        tree2.insert("Peter");
+        tree2.insert("Tom");
+        tree2.insert("Adam");
+        System.out.print("\nCurrent State of Tree1: ");
+        tree1.inorder();
+        System.out.print("\nCurrent State of Tree2: ");
+        tree2.inorder();
+        System.out.print("\nDoes Tree1 containsAll elements of Tree2? " + tree1.containsAll(tree2));
+        System.out.print("\nTree1 addAll Tree2: ");
+        tree1.addAll(tree2);
+        tree1.inorder();
+        System.out.print("\nTree2 retainAll Tree1: " );
+        tree2.retainAll(tree1);
+        tree2.inorder();
+        System.out.print("\nTree1 removeAll Tree2: " );
+        tree1.removeAll(tree2);
+        tree1.inorder();
+        System.out.print("\nCurrent State of Tree1: ");
+        tree1.inorder();
+        System.out.print("\nCurrent State of Tree2: ");
+        tree2.inorder();
+        System.out.print("\nTree1 as an Array: " + Arrays.toString(tree1.toArray()));
+        System.out.print("\nTree2 Cleared");
+        tree2.clear();
+        System.out.print("\nTree2 as an Array: " + Arrays.toString(tree2.toArray()));
+
+    }
+    /**
+     * Create a default binary tree
+     */
     public BST() {
     }
 
-    /** Create a binary tree from an array of objects */
+    /**
+     * Create a binary tree from an array of objects
+     */
     public BST(E[] objects) {
         for (int i = 0; i < objects.length; i++)
             add(objects[i]);
     }
 
-    @Override /** Returns true if the element is in the tree */
+    @Override
+    /** Returns true if the element is in the tree */
     public boolean search(E e) {
         TreeNode<E> current = root; // Start from the root
 
         while (current != null) {
             if (e.compareTo(current.element) < 0) {
                 current = current.left;
-            }
-            else if (e.compareTo(current.element) > 0) {
+            } else if (e.compareTo(current.element) > 0) {
                 current = current.right;
-            }
-            else // element matches current.element
+            } else // element matches current.element
                 return true; // Element is found
         }
 
         return false;
     }
 
-    @Override /** Insert element e into the binary tree
+    @Override
+    /** Insert element e into the binary tree
      * Return true if the element is inserted successfully */
     public boolean insert(E e) {
         if (root == null)
@@ -46,12 +254,10 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
                 if (e.compareTo(current.element) < 0) {
                     parent = current;
                     current = current.left;
-                }
-                else if (e.compareTo(current.element) > 0) {
+                } else if (e.compareTo(current.element) > 0) {
                     parent = current;
                     current = current.right;
-                }
-                else
+                } else
                     return false; // Duplicate node not inserted
 
             // Create the new node and attach it to the parent node
@@ -69,12 +275,15 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
         return new TreeNode<>(e);
     }
 
-    @Override /** Inorder traversal from the root */
+    @Override
+    /** Inorder traversal from the root */
     public void inorder() {
         inorder(root);
     }
 
-    /** Inorder traversal from a subtree */
+    /**
+     * Inorder traversal from a subtree
+     */
     protected void inorder(TreeNode<E> root) {
         if (root == null) return;
         inorder(root.left);
@@ -82,12 +291,15 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
         inorder(root.right);
     }
 
-    @Override /** Postorder traversal from the root */
+    @Override
+    /** Postorder traversal from the root */
     public void postorder() {
         postorder(root);
     }
 
-    /** Postorder traversal from a subtree */
+    /**
+     * Postorder traversal from a subtree
+     */
     protected void postorder(TreeNode<E> root) {
         if (root == null) return;
         postorder(root.left);
@@ -95,12 +307,15 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
         System.out.print(root.element + " ");
     }
 
-    @Override /** Preorder traversal from the root */
+    @Override
+    /** Preorder traversal from the root */
     public void preorder() {
         preorder(root);
     }
 
-    /** Preorder traversal from a subtree */
+    /**
+     * Preorder traversal from a subtree
+     */
     protected void preorder(TreeNode<E> root) {
         if (root == null) return;
         System.out.print(root.element + " ");
@@ -108,29 +323,22 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
         preorder(root.right);
     }
 
-    /** This inner class is static, because it does not access
-     any instance members defined in its outer class */
-    public static class TreeNode<E> {
-        public E element;
-        public TreeNode<E> left;
-        public TreeNode<E> right;
-
-        public TreeNode(E e) {
-            element = e;
-        }
-    }
-
-    @Override /** Get the number of nodes in the tree */
+    @Override
+    /** Get the number of nodes in the tree */
     public int getSize() {
         return size;
     }
 
-    /** Returns the root of the tree */
+    /**
+     * Returns the root of the tree
+     */
     public TreeNode<E> getRoot() {
         return root;
     }
 
-    /** Returns a path from the root leading to the specified element */
+    /**
+     * Returns a path from the root leading to the specified element
+     */
     public java.util.ArrayList<TreeNode<E>> path(E e) {
         java.util.ArrayList<TreeNode<E>> list =
                 new java.util.ArrayList<>();
@@ -140,18 +348,17 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
             list.add(current); // Add the node to the list
             if (e.compareTo(current.element) < 0) {
                 current = current.left;
-            }
-            else if (e.compareTo(current.element) > 0) {
+            } else if (e.compareTo(current.element) > 0) {
                 current = current.right;
-            }
-            else
+            } else
                 break;
         }
 
         return list; // Return an array list of nodes
     }
 
-    @Override /** Delete an element from the binary tree.
+    @Override
+    /** Delete an element from the binary tree.
      * Return true if the element is deleted successfully
      * Return false if the element is not in the tree */
     public boolean delete(E e) {
@@ -162,12 +369,10 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
             if (e.compareTo(current.element) < 0) {
                 parent = current;
                 current = current.left;
-            }
-            else if (e.compareTo(current.element) > 0) {
+            } else if (e.compareTo(current.element) > 0) {
                 parent = current;
                 current = current.right;
-            }
-            else
+            } else
                 break; // Element is in the tree pointed at by current
         }
 
@@ -179,15 +384,13 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
             // Connect the parent with the right child of the current node
             if (parent == null) {
                 root = current.right;
-            }
-            else {
+            } else {
                 if (e.compareTo(parent.element) < 0)
                     parent.left = current.right;
                 else
                     parent.right = current.right;
             }
-        }
-        else {
+        } else {
             // Case 2: The current node has a left child
             // Locate the rightmost node in the left subtree of
             // the current node and also its parent
@@ -214,15 +417,37 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
         return true; // Element deleted successfully
     }
 
-    @Override /** Obtain an iterator. Use inorder. */
+    @Override
+    /** Obtain an iterator. Use inorder. */
     public java.util.Iterator<E> iterator() {
         return new InorderIterator();
+    }
+
+    @Override
+    /** Remove all elements from the tree */
+    public void clear() {
+        root = null;
+        size = 0;
+    }
+
+    /**
+     * This inner class is static, because it does not access
+     * any instance members defined in its outer class
+     */
+    public static class TreeNode<E> {
+        public E element;
+        public TreeNode<E> left;
+        public TreeNode<E> right;
+
+        public TreeNode(E e) {
+            element = e;
+        }
     }
 
     // Inner class InorderIterator
     private class InorderIterator implements java.util.Iterator<E> {
         // Store the elements in a list
-        private java.util.ArrayList<E> list =
+        private final java.util.ArrayList<E> list =
                 new java.util.ArrayList<>();
         private int current = 0; // Point to the current element in list
 
@@ -230,28 +455,31 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
             inorder(); // Traverse binary tree and store elements in list
         }
 
-        /** Inorder traversal from the root*/
+        /**
+         * Inorder traversal from the root
+         */
         private void inorder() {
             inorder(root);
         }
 
-        /** Inorder traversal from a subtree */
+        /**
+         * Inorder traversal from a subtree
+         */
         private void inorder(TreeNode<E> root) {
-            if (root == null)return;
+            if (root == null) return;
             inorder(root.left);
             list.add(root.element);
             inorder(root.right);
         }
 
-        @Override /** More elements for traversing? */
+        @Override
+        /** More elements for traversing? */
         public boolean hasNext() {
-            if (current < list.size())
-                return true;
-
-            return false;
+            return current < list.size();
         }
 
-        @Override /** Get the current element and move to the next */
+        @Override
+        /** Get the current element and move to the next */
         public E next() {
             return list.get(current++);
         }
@@ -265,100 +493,5 @@ public class BST<E extends Comparable<E>> implements Tree<E> {
             list.clear(); // Clear the list
             inorder(); // Rebuild the list
         }
-    }
-
-    @Override /** Remove all elements from the tree */
-    public void clear() {
-        root = null;
-        size = 0;
-    }
-}
-
-interface Tree<E> extends Collection<E> {
-    /** Return true if the element is in the tree */
-    public boolean search(E e);
-
-    /** Insert element e into the binary tree
-     * Return true if the element is inserted successfully */
-    public boolean insert(E e);
-
-    /** Delete the specified element from the tree
-     * Return true if the element is deleted successfully */
-    public boolean delete(E e);
-
-    /** Get the number of elements in the tree */
-    public int getSize();
-
-    /** Inorder traversal from the root*/
-    public default void inorder() {
-    }
-
-    /** Postorder traversal from the root */
-    public default void postorder() {
-    }
-
-    /** Preorder traversal from the root */
-    public default void preorder() {
-    }
-
-    @Override /** Return true if the tree is empty */
-    public default boolean isEmpty() {
-        return this.size() == 0;
-    }
-
-    @Override
-    public default boolean contains(Object e) {
-        return search((E)e);
-    }
-
-    @Override
-    public default boolean add(E e) {
-        return insert(e);
-    }
-
-    @Override
-    public default boolean remove(Object e) {
-        return delete((E)e);
-    }
-
-    @Override
-    public default int size() {
-        return getSize();
-    }
-
-    @Override
-    public default boolean containsAll(Collection<?> c) {
-        // Left as an exercise
-        return false;
-    }
-
-    @Override
-    public default boolean addAll(Collection<? extends E> c) {
-        // Left as an exercise
-        return false;
-    }
-
-    @Override
-    public default boolean removeAll(Collection<?> c) {
-        // Left as an exercise
-        return false;
-    }
-
-    @Override
-    public default boolean retainAll(Collection<?> c) {
-        // Left as an exercise
-        return false;
-    }
-
-    @Override
-    public default Object[] toArray() {
-        // Left as an exercise
-        return null;
-    }
-
-    @Override
-    public default <T> T[] toArray(T[] array) {
-        // Left as an exercise
-        return null;
     }
 }
